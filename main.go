7460a018040121129a9f9f3e175d6d9132626238
Sören Gade <gade.soeren@gmail.com>
2015-12-19 00:57:26 +0100

    Added DB ORM.

--- main.go
+++ main.go
@@ -1,16 +1,59 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
 	"net/http"
+	"os"
 )
 
+var (
+	isProduction = false
+	db           gorm.DB
+)
+
+func handleError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
+	isProduction = (os.Getenv("PRODUCTION") != "")
+
+	initDB()
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 
 	http.Handle("/", r)
+	fmt.Printf("Serving...\n")
 	http.ListenAndServe(":3000", nil)
+}
+
+func initDB() {
+	var err error
+
+	if isProduction {
+		// heroku config
+		dbURL := ""
+		db, err = gorm.Open("postgres", dbURL)
+	} else {
+		db, err = gorm.Open("sqlite3", "./db.sqlite3")
+	}
+	if err != nil {
+		handleError(err)
+	}
+
+	err = db.DB().Ping()
+	if err != nil {
+		fmt.Printf("Db error: %v\n", err)
+	} else {
+		fmt.Printf("Database ready.\n")
+	}
 }
 
 func http_redirect(w http.ResponseWriter, newLocation string) {
