2651593c70b6f53e31993ab247dd9db1325e2ab8
Sören Gade <gade.soeren@gmail.com>
2015-12-21 15:54:19 +0100

    Added production database connection.

--- models/db.go
+++ models/db.go
@@ -1,27 +1,37 @@
 package models
 
 import (
+	"github.com/MoinApp/moinapp-server/global"
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
+	"log"
 )
 
 var db *gorm.DB
 
 func DB() *gorm.DB {
 	if db == nil {
-		//TODO: add production database connection
-		dbConnection, err := gorm.Open("sqlite3", "./db.sqlite3")
+		var dbConnection gorm.DB
+		var err error
+
+		if global.IsProduction() {
+			dbConnection, err = gorm.Open("postgres", global.GetDatabaseURL())
+		} else {
+			dbConnection, err = gorm.Open("sqlite3", "./db.sqlite3")
+		}
 
 		if err != nil {
 			//TODO: bad coding style
-			panic(err)
+			log.Fatalf("Could not connect to database: %v!\n", err)
 		}
 
 		db = &dbConnection
 
 		// add database table structs here
 		db.AutoMigrate(&User{})
+
+		db.LogMode(!global.IsProduction())
 	}
 
 	return db
