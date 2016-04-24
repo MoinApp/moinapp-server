de190c10b00d2c2ab9847be4004fd487b1dbbad6
Sören Gade <gade.soeren@gmail.com>
2016-04-24 13:10:35 +0200

    Including Travis' env variables in build script.

--- build.sh
+++ build.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-BRANCH=$(git symbolic-ref --short -q HEAD)
-if [ $BRANCH = "" ];
-  then
-  $BRANCH = git rev-parse HEAD
+if [[ $TRAVIS = true ]] && [[ $CI = true ]]; then
+  # if running on Travis CI
+
+  if [[ $TRAVIS_TAG ]]; then
+    # if building a tag
+    BRANCH=$TRAVIS_TAG
+  else
+    # else we are building a branch
+    BRANCH=$TRAVIS_BRANCH
+  fi
+else
+  # anywhere else, ask git!
+  BRANCH=$(git symbolic-ref --short -q HEAD)
+  # or if our head is detached, ask for the commit hash
+  if [[ $BRANCH = "" ]];
+    then
+    BRANCH = git rev-parse HEAD
+  fi
 fi
 
 go build -ldflags "-X main.APP_VERSION=$BRANCH"
